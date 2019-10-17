3ad0a674345b5fbfb005b4ec35f188d26d1629d3
Ramon Ayco Jr <Ramon.AycoJr@infor.com>
2019-10-18 05:03:23 +0800

    #11: refactor code to reduce complexities

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -1,7 +1,10 @@
 import { Minesweeper } from './minesweeper.js';
 
-var testMode = false; //Turn this variable to true to see where the mines are
+var testMode = true; //Turn this variable to true to see where the mines are
 var grid = document.createElement('table'); // to declare varible grid as type HTMLTableElement and get proper intellisense
 grid = document.getElementById("grid");
 var myMinesweeper = new Minesweeper(grid, testMode);
+window.reset = function() {
+ myMinesweeper.generateGrid();
+}
 myMinesweeper.generateGrid();
