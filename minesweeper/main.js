05f1366be6a3fdd6d4ce5fdb42ff332a76510a48
Ayo <ramon.aycojr@gmail.com>
2019-11-09 13:11:36 +0800

    #31 mines implementation and mobile support (#35)
    * #31: remove mines data from DOM
    
    * version

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -1,8 +1,12 @@
 import { Minesweeper } from './minesweeper.js';
 
-var testMode = false; //Turn this variable to true to see where the mines are
-var grid = document.createElement('table'); // to declare varible grid as type HTMLTableElement and get proper intellisense
+//Turn this variable to true to see where the mines are
+var testMode = true;
+
+// to declare varible grid as type HTMLTableElement and get proper intellisense
+var grid = document.createElement('table');
 grid = document.getElementById("grid");
+
 var myMinesweeper = new Minesweeper(grid, testMode);
 window.updateSetting = function() {
     let settingField = document.createElement('select');
