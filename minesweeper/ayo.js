8cfc2e00da4247f59f789967520afb77e50e83d8
Ayo <ramon.aycojr@gmail.com>
2019-12-08 18:30:45 +0800

    clean up

--- minesweeper/ayo.js
+++
@@ -1,7 +0,0 @@
-import { LeaderBoardService } from '../services/leader-board.service.js';
-
-const leaderBoard = new LeaderBoardService('mw-leaders', 'mw-all');
-
-window.updateLeaders = () => {
-    leaderBoard.updateTimeStampsLeaders();
-}
