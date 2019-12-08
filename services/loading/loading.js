9904f1edb527993a34e6cd469197520cd9d0a6ed
Ayo <ramon.aycojr@gmail.com>
2019-12-08 18:27:23 +0800

    remove minesweeper

--- services/loading/loading.js
+++
@@ -1,8 +0,0 @@
-export class LoadingService {
-    addLoading(element) {
-        element.innerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
-    }
-    removeLoading(element) {
-        element.innerHTML = '';
-    }
-}
